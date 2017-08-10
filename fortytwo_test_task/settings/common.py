f3aa8245d67c3a5d6c43267d9f27a5fc7cb34ca8
Esknor <vldaimir2016@mail.ua>
2017-08-10 22:11:14 +0300

    My Ticket#1 and Ticket#2 commit

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.hello',
+	'loginsys',
 )
 
 MIDDLEWARE_CLASSES = (
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+import loginsys
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -8,6 +9,6 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('loginsys.urls')),	
 )
